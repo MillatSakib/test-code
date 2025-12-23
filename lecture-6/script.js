33fdcb0396053cd4affe95bbaeb58e1eb90f32fc
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2025-12-23 19:58:03 +0600

    lecture 6 added

---
+++ lecture-6/script.js
@@ -0,0 +1,25 @@
+console.log("Hello");
+
+var aoo = { name: "sakib", age: 22, id: 222902036 };
+
+console.log(aoo);
+
+aoo = JSON.stringify(aoo);
+
+console.log(aoo);
+
+aoo = JSON.parse(aoo);
+
+console.log(aoo);
+
+var arr = ["abc", "def", "ghi"];
+
+console.log(arr);
+
+arr = JSON.stringify(arr);
+
+console.log(arr);
+
+arr = JSON.parse(arr);
+
+console.log(arr);
